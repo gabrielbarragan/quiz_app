d8a29fef26a8a4089d132d97330f62feedd664bc
gabo <gabrielbarragan07@gmail.com>
2021-09-14 21:00:41 -0500

    Creando los primeros templates, quiz.html y main.html

--- quizes/views.py
+++ quizes/views.py
@@ -8,6 +8,7 @@ class QuizListView (ListView):
     '''Quiz view'''
     model = Quiz
     template_name = 'quizes/main.html'
+    context_object_name= 'quizes'
 
 def quiz_view(request, pk):
     quiz= Quiz.objects.get(pk=pk)

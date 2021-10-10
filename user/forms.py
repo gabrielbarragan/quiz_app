36c9f8ebd9f0d47f5950a153368f4936c5aefc79
gabo <gabrielbarragan07@gmail.com>
2021-10-10 13:28:23 -0500

    se termina con el registro para nuevos usuarios

--- user/forms.py
+++ user/forms.py
@@ -6,7 +6,50 @@ from django.contrib.auth.models import User
 
 from user.models import Player
 
+class SignupForm(forms.Form):
+    """sing up form"""
+    username = forms.CharField(min_length=4, max_length=50)
+
+    password = forms.CharField(
+        min_length=6,
+        max_length=70,
+        widget=forms.PasswordInput()
+    )
+    confirmation_password = forms.CharField(
+        min_length=6,
+        max_length=70,
+        widget=forms.PasswordInput()
+    )
+
+    first_name= forms.CharField(min_length=2,max_length=50)
+    last_name= forms.CharField(min_length=2,max_length=50)
+    email = forms.CharField(
+        min_length=6,
+        max_length=70,
+        widget=forms.EmailInput()
+    )
+
+    def clean_username(self):
+        """username mustbe unique"""
+
+        username = self.cleaned_data['username']
+        username_taken = User.objects.filter(username=username).exists()
+
+        if username_taken:
+            raise forms.ValidationError('El usuario ya existe.')
+        return username
+    def save(self):
+        """Create and save user and Player"""
+        data = self.cleaned_data
+        data.pop('confirmation_password')
+
+        user = User.objects.create_user(**data)
+        player = Player(user=user)
+        player.save()
+
 class PlayerForm(forms.Form):
+    """Player form """
     
-    biography = forms.CharField(max_length=500,required=False)
+    biography = forms.CharField(max_length=20,required=False)
     picture = forms.ImageField()
+
--- user/views.py
+++ user/views.py
@@ -3,11 +3,16 @@
 from django.shortcuts import render
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.mixins import LoginRequiredMixin
-# Create your views here.
+from django.views.generic.edit import FormView, UpdateView
+from django.urls import reverse, reverse_lazy
 
 #models
-
 from user.models import User, Player
+
+#forms
+from user.forms import SignupForm
+
+# Create your views here.
 class LoginView(auth_views.LoginView):
     """Login user view"""
     
@@ -16,3 +21,15 @@ class LoginView(auth_views.LoginView):
 class LogoutView(LoginRequiredMixin,auth_views.LogoutView):
     """Users Logout view"""
     next_page= 'user:login'
+
+class SignupView(FormView):
+    """user signup view"""
+    form_class = SignupForm
+    template_name = 'user/new_user.html'
+    success_url = reverse_lazy('user:login')
+
+    def form_valid(self, form):
+        """save form data"""
+        form.save()
+
+        return super().form_valid(form)
